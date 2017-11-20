e318bf5e1330b2303d536787245bce21b29c40da
William Garrido <will.bypass@gmail.com>
2017-11-20 16:54:01 +0100

    feat: add new route for component

--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -11,8 +11,9 @@ export default new Router({
       component: require('@/components/LandingPage').default
     },
     {
-      path: '*',
-      redirect: '/'
+      path: '/config',
+      name: 'config',
+      component: require('@/components/Config').default
     }
   ]
 })
