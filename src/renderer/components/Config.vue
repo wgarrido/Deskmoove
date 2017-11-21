b8cf227a2e2aa72b15c2a5b58a7386b9d79b8417
William Garrido <will.bypass@gmail.com>
2017-11-21 15:53:30 +0100

    feat : add photon css library

--- src/renderer/components/Config.vue
+++ src/renderer/components/Config.vue
@@ -1,47 +1,81 @@
 <template>
-  <div id="config-wrapper">
-    <div id="container-btn">
-      <button class="btn">OK</button>
-      <button class="btn">CANCEL</button>
-      <button class="btn" id="btn-quit">QUIT</button>
+  <div id="container">
+    <header class="toolbar toolbar-header">
+      <h1 class="title">Configuration</h1>
+      <div class="toolbar-actions">
+      <button class="btn btn-default active">
+        <span class="icon icon-video"></span>
+      </button>
+  </div>
+    </header>
+  <div id="content">
+    <div id="video">
+      <div class="radio">
+        <label>
+          <input type="radio" name="radios" value="local" v-model="videoConfig">
+          local video
+        </label>
+        <label>
+          <input type="radio" name="radios" value="internet" v-model="videoConfig">
+          internet video
+        </label>
+      </div>
+      <div v-if="videoConfig === 'local'">
+        <button class="btn btn-large btn-default" @click="loadVideoFile()">Load mp4 file</button>
+        <span>file: {{localVideo}}</span>
+      </div>
+      <div v-if="videoConfig === 'internet'">
+        <div class="form-group">
+          <input type="url" class="form-control" placeholder="Enter Youtube or Vimeo link">
+        </div>
+      </div>
     </div>
+  </div>
+    <footer class="toolbar toolbar-footer">
+      <div class="toolbar-actions">
+        <button class="btn btn-default">
+          Cancel
+        </button>
+
+        <button class="btn btn-primary pull-right">
+          Save
+        </button>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script>
+  import { remote, ipcRenderer } from 'electron'
   export default {
-    data () {}
+    data () {
+      return {
+        videoConfig: 'local',
+        localVideo: 'empty'
+      }
+    },
+    methods: {
+      loadVideoFile () {
+        remote.dialog.showOpenDialog({
+          properties: ['openFile'],
+          filters: [{name: 'Movies', extensions: ['mp4']}]
+        }, (path) => {
+          // Send message on Main channel
+          ipcRenderer.send('videoSrc', [path, 'local'])
+          this.localVideo = path[0].substr(path[0].lastIndexOf('/') + 1)
+        })
+      }
+    }
   }
 </script>
 
 <style>
-  body{
-    background-color: #313131;
-  }
-  .btn{
-    width: 120px;
-    height: 38px;
-    border: 1px solid #FFFFFF;
-    font-size: 15px;
-    color: #FFFFFF;
-    background-color: transparent;
-    cursor: pointer
-  }
+@import "~@/assets/css/photon.min.css";
+body {
+  resize: none
+}
 
-  #config-wrapper{
-    position: relative;
-    min-height: 180px;
-  }
-
-  #container-btn{
-    position: absolute;
-    bottom: 5px;
-    left: 5px;
-  }
-
-  #btn-quit{
-    border: 1px solid #F67648;
-  }
-
-
+#content {
+  padding: 15px
+}
 </style>
