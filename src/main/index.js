dfef84557cf9ba048f812bab54364c60b750787e
William Garrido <will.bypass@gmail.com>
2017-11-21 15:54:38 +0100

    style : remove comment

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { app, BrowserWindow, screen, Tray, nativeImage } from 'electron'
+import { app, BrowserWindow, screen, Tray, nativeImage, ipcMain } from 'electron'
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
@@ -48,22 +48,6 @@ app.on('ready', () => {
   console.log(__static)
   const nicon = nativeImage.createFromPath(`${__static}/img/icon.png`)
   tray = new Tray(nicon)
-  /* const contextMenu = Menu.buildFromTemplate([
-    {
-      label: 'load',
-      type: 'normal',
-      click: () => {
-        dialog.showOpenDialog({
-          properties: ['openFile'],
-          filters: [{name: 'Movies', extensions: ['mp4']}]
-        }, (path) => {
-          // Send message on change-src channel
-          mainWindow.webContents.send('change-src', [path, 'local'])
-        })
-      }
-    },
-    {label: 'quit', type: 'normal', role: 'quit'}
-  ]) */
   tray.setToolTip('VideoDesk')
   // tray.setContextMenu(contextMenu)
   tray.on('click', () => {
@@ -71,12 +55,13 @@ app.on('ready', () => {
       paramWindow = new BrowserWindow({
         y: screen.getMenuBarHeight(),
         x: screen.getCursorScreenPoint().x - 200,
-        height: 200,
         width: 400,
+        minWidth: 400,
+        maxWidth: 400,
         frame: false,
         useContentSize: true,
         webPreferences: {
-          devTools: false
+          devTools: true
         }
       })
       paramWindow.loadURL(configURL)
@@ -86,6 +71,13 @@ app.on('ready', () => {
     }
   })
   createWindow(width, height)
+
+  // IPC
+  ipcMain.on('videoSrc', (event, arg) => {
+    console.log(event, arg)
+    // Send message on change-src channel
+    mainWindow.webContents.send('change-src', [arg[0], arg[1]])
+  })
 })
 
 app.on('window-all-closed', () => {
