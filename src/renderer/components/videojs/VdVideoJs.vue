cefb8508d09ed980dfbc42f0863290dae2d3d0c5
William Garrido <will.bypass@gmail.com>
2017-11-22 11:07:22 +0100

    feat : component vd-video-js

---
+++ src/renderer/components/videojs/VdVideoJs.vue
@@ -0,0 +1,41 @@
+<template>
+<div id="container-player">
+  <video id="my-player" class="video-js" preload="auto" :data-setup="dataSetup" v-show="context === 'external'"></video>
+  <video id="my-player" class="video-js" preload="auto" v-show="context === 'local'">
+      <source :src="srcVideo" type="video/mp4">
+  </video>
+</div>
+</template>
+
+<script>
+// import VideoJs from 'video.js'
+export default {
+  props: {
+    srcVideo: String,
+    dataSetup: String,
+    context: String
+  },
+  data () {
+    return {
+      player: null,
+      optionsPlayer: {
+        autoplay: true,
+        controls: false,
+        loop: true,
+        muted: true,
+        preload: 'auto',
+        fluid: true
+      }
+    }
+  },
+  mounted: function () {
+    // this.player = VideoJs('my-player', this.optionsPlayer)
+  },
+  beforeDestroy: function () {
+    this.player.dispose()
+  }
+}
+</script>
+
+<style>
+</style>
